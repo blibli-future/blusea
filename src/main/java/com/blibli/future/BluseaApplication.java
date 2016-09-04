0a8042d634c01baba099a917f96ece8c2d2623b7
Adhika Setya Pramudita <adhika.setyap@gmail.com>
2016-09-04 16:49:57 +0700

    Enable static resource mapping

--- src/main/java/com/blibli/future/BluseaApplication.java
+++ src/main/java/com/blibli/future/BluseaApplication.java
@@ -2,11 +2,19 @@ package com.blibli.future;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class BluseaApplication {
-	
+public class BluseaApplication extends WebMvcConfigurerAdapter {
+
 	public static void main(String[] args) {
 		SpringApplication.run(BluseaApplication.class, args);
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/assets/**")
+				.addResourceLocations("classpath:/static/");
 	}
 }
