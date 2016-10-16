dd1983a6747c9345738b89d146e144548ce2cc0b
Adhika Setya Pramudita <adhika.setyap@gmail.com>
2016-10-16 15:06:50 +0700

    Create profile page and login form

--- src/main/java/com/blibli/future/controller/UserController.java
+++ src/main/java/com/blibli/future/controller/UserController.java
@@ -2,7 +2,10 @@ package com.blibli.future.controller;
 
 import com.blibli.future.model.User;
 import com.blibli.future.repository.UserRepository;
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +18,14 @@ public class UserController {
     @Autowired
     private UserRepository repo;
 
+    @RequestMapping("/user/profile")
+    public String profile(Model model)
+    {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        model.addAttribute("user", user);
+        return "/user/dashboard";
+    }
+
     @RequestMapping("/user/{userId}")
     public String show(
             @PathVariable Long userId,
@@ -25,13 +36,24 @@ public class UserController {
     }
 
     @RequestMapping(value="register", method= RequestMethod.POST)
-    public String landing(
+    public String register(
             @ModelAttribute User newUser,
             Model model)
     {
         repo.save(newUser);
         model.addAttribute("user", newUser);
         return "redirect:/user/" + newUser.getId();
+    }
+
+    @RequestMapping(value="/user/login", method= RequestMethod.GET)
+    public String login(
+            @ModelAttribute User newUser,
+            HttpServletRequest request,
+            Model model)
+    {
+        String _csrf = ((CsrfToken) request.getAttribute("_csrf")).getToken();
+        model.addAttribute("_csrf", _csrf);
+        return "user/login";
     }
 
 }
