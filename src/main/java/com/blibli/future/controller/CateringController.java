47f6861ef4aabb4abb11e96c40093a06a4d2d428
Ahmad Widardi <awidardi@gmail.com>
2016-10-23 17:03:18 +0700

    Added - Upload File Feature

--- src/main/java/com/blibli/future/controller/CateringController.java
+++ src/main/java/com/blibli/future/controller/CateringController.java
@@ -4,6 +4,8 @@ package com.blibli.future.controller;
  * Created by ARDI on 10/6/2016.
  */
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.stereotype.Controller;
@@ -14,6 +16,13 @@ import com.blibli.future.model.Product;
 import com.blibli.future.model.Catering;
 import com.blibli.future.repository.CateringRepository;
 import com.blibli.future.repository.ProductRepository;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 @Controller
 public class CateringController {
@@ -23,6 +32,9 @@ public class CateringController {
 
     @Autowired
     ProductRepository productRepository;
+
+    private static final Logger logger = LoggerFactory
+            .getLogger(CateringController.class);
 
     @RequestMapping("/catering/{username}")
     public String cateringProfile(
@@ -84,12 +96,48 @@ public class CateringController {
     public String cateringPostAddProduct(
             @PathVariable String username,
             @ModelAttribute Product newProduct,
+            @RequestParam("file") MultipartFile file,
             Model model){
         Catering catering = cateringRepository.findByUsername(username);
+
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd");
+        String formatted = format1.format(cal.getTime());
 
         if(catering != null){
             if(!catering.hasProduct(newProduct)){
                 newProduct.setCatering(catering);
+            }
+            if (!file.isEmpty()) {
+                try {
+                    byte[] bytes = file.getBytes();
+
+                    // Creating the directory to store file
+                    //String rootPath = System.getProperty("catalina.home");
+                    File dir = new File("D:\\Software\\xampp\\htdocs\\gambar\\" + formatted);
+                    if (!dir.exists())
+                        dir.mkdirs();
+
+                    // Create the file on server
+                    File serverFile = new File(dir.getAbsolutePath()
+                            + File.separator + newProduct.getId() + ".jpg");
+                    newProduct.setPhoto("gambar"
+                            + File.separator + formatted + File.separator + newProduct.getId() + ".jpg");
+                    BufferedOutputStream stream = new BufferedOutputStream(
+                            new FileOutputStream(serverFile));
+                    stream.write(bytes);
+                    stream.close();
+
+                    logger.info("Server File Location="
+                            + serverFile.getAbsolutePath());
+
+//                    return "You successfully uploaded file=" + newProduct.getName();
+                } catch (Exception e) {
+                    return "You failed to upload " + newProduct.getName() + " => " + e.getMessage();
+                }
+            } else {
+                return "You failed to upload " + newProduct.getName()
+                        + " because the file was empty.";
             }
             productRepository.save(newProduct);
             cateringRepository.save(catering);
