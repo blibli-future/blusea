dca6b870ccd40aa73d1c3e7498f950d377e06316
Adhika Setya Pramudita <adhika.setyap@gmail.com>
2016-11-12 00:40:30 +0700

    Create very crude catering list on home page

--- src/main/java/com/blibli/future/controller/PublicController.java
+++ src/main/java/com/blibli/future/controller/PublicController.java
@@ -1,16 +1,26 @@
 package com.blibli.future.controller;
 
+import com.blibli.future.model.Catering;
+import com.blibli.future.repository.CateringRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class PublicController {
+
+	@Autowired
+	CateringRepository cateringRepository;
 
 	@RequestMapping("/")
 	public String landing(
 			Model model)
 	{
+		List<Catering> allCatering = ((List<Catering>) cateringRepository.findAll());
+		model.addAttribute("caterings", allCatering);
 		return "landing";
 	}
 }
