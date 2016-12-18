2dd5138e74aab4704d736e16be02919c0b9a71e3
Ahmad Widardi <awidardi@gmail.com>
2016-12-18 15:56:11 +0700

    Fix uploading picture's bug

--- src/main/java/com/blibli/future/controller/CateringController.java
+++ src/main/java/com/blibli/future/controller/CateringController.java
@@ -115,7 +115,7 @@ public class CateringController {
                     // Create the file on server
                     File serverFile = new File(dir.getAbsolutePath()
                             + File.separator + fileName + ".jpg");
-                    newProduct.setPhoto("gambar"
+                    newProduct.setPhoto("http://localhost/gambar"
                             + File.separator + formatted + File.separator + fileName + ".jpg");
                     BufferedOutputStream stream = new BufferedOutputStream(
                             new FileOutputStream(serverFile));
