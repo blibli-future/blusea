97819261ddbc213a0ddad2ee23c4a96f7048f82c
Ahmad Widardi <awidardi@gmail.com>
2016-12-20 00:20:16 +0700

    Added - Logout - Catering navbar

--- src/main/java/com/blibli/future/security/WebSecurityConfig.java
+++ src/main/java/com/blibli/future/security/WebSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import javax.sql.DataSource;
 
@@ -48,8 +49,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .failureUrl("/login-error.html")
                     .permitAll()
                 .and()
-                .logout()
-                .logoutSuccessUrl("/public/landing");
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/");
     }
 
     @Autowired
